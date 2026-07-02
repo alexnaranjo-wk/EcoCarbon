6bcb686381b970763187705b31f2c85d57e03eca
alexnaranjo-wk <geovannymasias1991iste@gmail.com>
2026-07-01 19:05:26 -0500

    Agregar archivo test_core.py

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,1 +1,16 @@
-# Pruebas del módulo Core
+from src.núcleo import CarbonFootprintCalculator
+
+
+def test_calculate_co2():
+    calculator = CarbonFootprintCalculator()
+    result = calculator.calculate_co2(350)
+    assert result == 134.75
+
+
+def test_negative_consumption():
+    calculator = CarbonFootprintCalculator()
+
+    try:
+        calculator.calculate_co2(-10)
+    except ValueError as error:
+        assert str(error) == "El consumo no puede ser negativo."
